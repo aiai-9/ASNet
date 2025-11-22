d85e3fdcf4a4bcbd4b1e1e1006a82f23066c2fb7
AiAI-99 <aiairesearch7@gmail.com>
2025-11-22 18:26:40 +0000

    remove unwanted folders

--- audioshieldnet/data/prepare_data/prepare_run.sh
+++
@@ -1,9 +0,0 @@
-# libriSeVoc
-
-python audioShieldNet/asnet_3/audioshieldnet/data/prepare_data/librisevoc_prepare.py \
-  --root_dir  /scratch/xxxxxx/projects/deepfake/dataset/audio/LibriSeVoc \
-  --lists_dir /scratch/xxxxxx/projects/deepfake/dataset/audio/LibriSeVoc/lists \
-  --train_list train.list \
-  --val_list   dev.list \
-  --test_list  test.list \
-  --out_dir    /scratch/xxxxxx/projects/deepfake/dataset/audio/LibriSeVoc/combined
